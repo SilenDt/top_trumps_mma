225c55a5f27fcd47cc2c3b1381eea32147608cc5
Eilidh Morone <e.silendt@gmail.com>
2023-02-02 15:24:05 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Top Trumps MMA</h1>
+    <h2>Eilidh</h2>
+    <h2>Sam</h2>
+    <h2>Double Fraser</h2>
     </div>
   );
 }
