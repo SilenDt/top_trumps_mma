005b962f5689dfd83566af23672a5e692cda7ada
Fraser <fraser.russell1991@gmail.com>
2023-02-07 14:52:09 +0000

    added game info: shows current player and turn info

---
+++ client/src/components/GameInfoDisplay.js
@@ -0,0 +1,45 @@
+
+const GameInfoDisplay = ({currentPlayer, roundWinner}) => {
+    
+    const showWhosTurn = (currentPlayer) => {
+        let whosTurnInfo = ""
+        if (currentPlayer === "player") {
+            whosTurnInfo = `${currentPlayer}: It's your turn, pick a stat on your card!`
+        } else {
+            whosTurnInfo = `${currentPlayer}: It's the computers turn, watch out he's fierce!`
+        }
+        return whosTurnInfo
+    }
+    const showLastRoundWinner = (roundWinner) => {
+        let roundWinnerInfo = ""
+        if (roundWinner === 'first round') {
+            roundWinnerInfo = (
+                `first round, fight!`
+            )
+        }  else if (roundWinner === 'player') {
+            roundWinnerInfo = (
+                `the last round was won by the ${roundWinner}!`
+            )
+        } else if (roundWinner === 'computer') {
+            roundWinnerInfo = (
+                `the last round was won by the ${roundWinner}!`
+            )
+        } else if (roundWinner === 'draw') {
+            roundWinnerInfo = (
+            `the last round was a ${roundWinner}`
+            )
+        }
+        return roundWinnerInfo
+    }
+    return (
+        <div>
+        <h4>Turn</h4>
+        {showWhosTurn(currentPlayer)}
+        <h4>Last round</h4>
+        {showLastRoundWinner(roundWinner)}
+
+        </div>
+    )
+}
+
+export default GameInfoDisplay
--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -3,6 +3,7 @@ import arrayShuffle from 'array-shuffle';
 import { useState, useEffect } from 'react';
 import PlayerContainer from "./PlayerContainer";
 import ComputerContainer from "./ComputerContainer";
+import GameInfoDisplay from "../components/GameInfoDisplay";
 
 const MainContainer = () => {
 
@@ -22,7 +23,7 @@ const MainContainer = () => {
     const [chosenStat, setChosenStat] = useState(null)
 
     const [isPlayerCardHigher, setIsPlayerCardHigher]  = useState(null) //boolean
-    const [roundWinner, setRoundWinner] = useState(null) 
+    const [roundWinner, setRoundWinner] = useState("first round") 
 
     const [stateOfPlay, setStateOfPlay] = useState('loading') //'loading', 'inPlay', 'victory', 'defeat'
 
@@ -136,7 +137,6 @@ const MainContainer = () => {
 
         // player wins round
         if (currentPlayerValue > currentComputerValue) {
-        console.log("players card is higher")
         console.log(currentPlayerValue)
         console.log(currentComputerValue)
             //reveal opponents card
@@ -170,6 +170,7 @@ const MainContainer = () => {
         //draw next card
         setCurrentPlayerCard(playerDeck[0])
         setCurrentComputerCard(computerDeck[0])
+        setRoundWinner("player")
 
         // computer wins the round
         } else if (currentPlayerValue < currentComputerValue) {
@@ -199,6 +200,7 @@ const MainContainer = () => {
         //draw next card
         setCurrentPlayerCard(playerDeck[0])
         setCurrentComputerCard(computerDeck[0])
+        setRoundWinner("computer")
 
         // computer wins so gets a turn
         // 'computer-again' required to register change in useEffect and continue computers turn
@@ -211,7 +213,6 @@ const MainContainer = () => {
         // cards are equal!
         } else if 
             (currentPlayerValue === currentComputerValue){
-                console.log("draw")
                 const copyOfPlayerDeck = [...playerDeck]
                 const arrayOfPlayerCard = copyOfPlayerDeck.splice(0,1)
                 const playerCard = arrayOfPlayerCard[0]
@@ -226,6 +227,7 @@ const MainContainer = () => {
                 //setComputerDeck to new copy 
                 setComputerDeck(copyOfComputerDeck)
                 console.log("values equal")
+                setRoundWinner("draw")
         }
         return true
     }
@@ -266,6 +268,10 @@ const MainContainer = () => {
             <div className="player-deck">
                 <h2>Computer Deck</h2>
                 {renderEachPlayersContainer("computer")}
+            </div>
+            <div className="game-info"> 
+            <h2>Game Info</h2>
+            <GameInfoDisplay currentPlayer={currentPlayer} roundWinner={roundWinner}/>
             </div>
             <div className="player-deck">
             <h2>Player Deck</h2>
